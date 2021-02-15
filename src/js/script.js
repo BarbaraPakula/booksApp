ce3af2f000f77fe9ed1562440d48beffce4aa0ac
Barbara Pakula <barbara.pakula@hotmail.com>
2021-02-15 11:08:59 +0100

    add js books filter

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@
     },
     containerOf: {
       bookList: '.books-list',
+      form: '.filters',
     },
     book: {
       image: '.books-list .book__image',
@@ -18,7 +19,9 @@
     booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
   };
   const menuContainer = document.querySelector(select.containerOf.bookList);
+  const formHtmlFiltered = document.querySelector(select.containerOf.form);
   const favoriteBooks = [];
+  const filters = [];
 
 
   const render = function () {
@@ -47,11 +50,49 @@
           elem.classList.add('favorite');
           favoriteBooks.push(id);
         }
-        console.log(favoriteBooks);
+        // console.log(favoriteBooks);
       }
     });
 
+    formHtmlFiltered.addEventListener('change', function (event) {
+      event.preventDefault();
+      const elem = event.target;
+      if (elem.type === 'checkbox') {
+        if (elem.checked) {
+          filters.push(elem.value);
+          console.log(filters);
+        } else {
+          const indexOfFilter = filters.indexOf(elem.value);
+          filters.splice(indexOfFilter, 1);
+          console.log(filters);
+        }
+      }
+      filterBooks();
+    });
 
+
+  }
+  function filterBooks() {
+    for (let elem of dataSource.books) {
+      // console.log('elem',elem);
+      let shouldBeHidden = false;
+
+      for (let filter of filters) {
+        if (!elem.details[filter]) {
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      if (shouldBeHidden) {
+        const book = document.querySelector('.book__image[data-id="' + elem.id + '"]');
+        console.log( 'book', book);
+        console.log('elem' ,elem.id);
+        book.classList.add('hidden');
+      } else {
+        const book = document.querySelector('.book__image[data-id="' + elem.id + '"]');
+        book.classList.remove('hidden');
+      }
+    }
   }
 
   render();
